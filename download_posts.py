1f956d21b8941e412d6e9df147eb43ccc1f725c4
Josiah Carlson <josiah.carlson@gmail.com>
2018-12-21 20:29:31 -0800

    Make post download secure.

--- download_posts.py
+++ download_posts.py
@@ -12,7 +12,7 @@ YEARS = range(2003, 2015)  # first to (last + 1)
 
 def fetch_month_posts(year, month):
     response = requests.post(
-        'http://www.livejournal.com/export_do.bml',
+        'https://www.livejournal.com/export_do.bml',
         headers=headers,
         cookies=cookies,
         data={
