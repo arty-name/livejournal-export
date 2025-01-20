4e7d8d04fb608746f8ee24b79d7e08941261a287
Jennifer Hicks <jenniferhicks@GERTY.local>
2025-01-20 18:31:55 -0500

    start and end dates instead of years

--- download_posts.py
+++ download_posts.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 
 import json
-import os 
-import requests 
+import os
+import requests
 from sys import exit as sysexit
 import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
-# At time of script import, request range of years. We'll add 1 to the end year automatically.
-try:
-    startYear = int(input("Enter first year you want to export: "))
-except Exception as e:
-    print(f"\nError with first year entered. Error: {e}. Exiting...")
-    sysexit(1)
+POSTS_XML_DIR = 'posts-xml'
+POSTS_JSON_DIR = 'posts-json'
+DATE_FORMAT = '%Y%m%d'
 
-try:
-    endYear = int(input("Enter last year you want to export: "))
-except Exception as e:
-    print(f"\nError with last year entered. Error: {e}. Exiting...")
-    sysexit(1)
 
-YEARS = range(startYear, endYear + 1)  # first to (last + 1)
+def get_date_input(prompt):
+    try:
+        start_date = int(input("Enter start date in YYYYMMDD format: "))
+    except Exception as e:
+        print(f"\nError: {e}. Exiting...")
+        sysexit(1)
+
+    try:
+        end_date = int(input("Enter end date in YYYYMMDD format: "))
+    except Exception as e:
+        print(f"\nError: {e}. Exiting...")
+        sysexit(1)
+    return start_date, end_date
 
 def fetch_month_posts(year, month, cookies, headers):
     response = requests.post(
@@ -43,7 +49,7 @@ def fetch_month_posts(year, month, cookies, headers):
             'field_currents': 'on'
         }
     )
-
+    print(response.text)
     return response.text
 
 def xml_to_json(xml):
@@ -62,18 +68,30 @@ def xml_to_json(xml):
         'current_mood': f('current_mood')
     }
 
+def increment_month(date):
+    return date + relativedelta(months=1, day=1)
+
 def download_posts(cookies, headers):
     os.makedirs('posts-xml', exist_ok=True)
     os.makedirs('posts-json', exist_ok=True)
 
+    # Get start and end dates with validation
+    start_date, end_date =  get_date_input("Enter start date in YYYYMMDD format: ")
+    processing_datetime = datetime.strptime(str(start_date), DATE_FORMAT)
+    end_date = datetime.strptime(str(end_date), DATE_FORMAT) #converting to datetime object
     xml_posts = []
-    for year in YEARS:
-        for month in range(1, 13):
-            xml = fetch_month_posts(year, month, cookies, headers)
-            xml_posts.extend(list(ET.fromstring(xml).iter('entry')))
 
-            with open('posts-xml/{0}-{1:02d}.xml'.format(year, month), 'w+', encoding='utf-8') as file:
-                file.write(xml)
+    while processing_datetime <= end_date:
+        year = processing_datetime.year
+        month = processing_datetime.month
+
+        xml = fetch_month_posts(year, month, cookies, headers)
+        xml_posts.extend(list(ET.fromstring(xml).iter('entry')))
+
+        with open('posts-xml/{0}-{1:02d}.xml'.format(year, month), 'w+', encoding='utf-8') as file:
+            file.write(xml)
+        
+        processing_datetime = increment_month(processing_datetime)  # Move to next month
 
     json_posts = list(map(xml_to_json, xml_posts))
     with open('posts-json/all.json', 'w', encoding='utf-8') as f:
