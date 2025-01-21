7536afa54a8fd1d009512ae2bce7fdfcb3ab0ad1
Jennifer Hicks <jenniferhicks@GERTY.local>
2025-01-21 15:16:51 -0500

    dates instead of years

--- download_posts.py
+++ download_posts.py
@@ -8,6 +8,8 @@ import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
+DATE_FORMAT = '%Y%m%d'
+
 def get_date_input(prompt):
     try:
         start_date = int(input("Enter start date in YYYYMMDD format: "))
